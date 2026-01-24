7bee15f594f6a63af5b095ad57d4a2bb00f80eb4
clown1138 <clown1138@gmail.com>
2026-01-25 02:26:43 +0800

    允許屬性

---
+++ global.d.ts
@@ -0,0 +1,10 @@
+declare namespace JSX {
+    interface IntrinsicElements {
+      'a-scene': any;
+      'a-assets': any;
+      'a-camera': any;
+      'a-entity': any;
+      'a-plane': any;
+      'a-img': any;
+    }
+  }
